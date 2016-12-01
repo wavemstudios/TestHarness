8ea84b55cbbf7771afe8d7ec816a7f4cd8bbfc1d
wavemstudios <steve@wavemstudios.com>
2016-12-01 14:16:38 +0000

    client update

--- apduClient.c
+++ apduClient.c
@@ -7,10 +7,11 @@
  *      APDU Client
  */
 
-#include<stdio.h> //printf
-#include<string.h>    //strlen
-#include<sys/socket.h>    //socket
-#include<arpa/inet.h> //inet_addr
+#include <stdio.h> //printf
+#include <string.h>    //strlen
+#include <sys/socket.h>    //socket
+#include <arpa/inet.h> //inet_addr
+#include <sys/time.h>
 
 int main(int argc , char *argv[])
 {
@@ -18,7 +19,7 @@ int main(int argc , char *argv[])
     int resend = 0;
     int idx;
     struct sockaddr_in server;
-    char message[1000], server_reply[200], command[10];
+    unsigned char message[1000], server_reply[200], command[10];
     unsigned char tlvtest1[] = {0x5F,0x81,0x81,0x01,0x02,0x00,0x00}; //Antenna Off
     unsigned char tlvtest2[] = {0x5F,0x81,0x81,0x01,0x06,0x04,0x00,0x01,0x02,0x03,0x04}; //Load Polling Table
     unsigned char tlvtest3[] = {0x5F,0x81,0x81,0x01,0x06,0x02,0x00,0x01,0x02,0x03,0x04}; //Poll For Card
@@ -44,7 +45,7 @@ start:
  	   perror("setsockopt SO_REUSEADDR. Error");
     }
 
-    server.sin_addr.s_addr = inet_addr("192.168.203.74");
+    server.sin_addr.s_addr = inet_addr("192.168.203.8");
     server.sin_family = AF_INET;
     server.sin_port = htons( 8888 );
 
@@ -60,29 +61,59 @@ start:
     //keep communicating with server
     while(1)
     {
-    	if (resend == 0){
-    		printf("Select Command \n");
-    		printf("1) 0x5F,0x81,0x81,0x01,0x02,0x00,0x00 //Antenna Off \n");
-    		printf("2) 0x5F,0x81,0x81,0x01,0x06,0x04,0x00,0x01,0x02,0x03,0x04 //Load Polling Table \n");
-    		printf("3) 0x5F,0x81,0x81,0x01,0x06,0x02,0x00,0x01,0x02,0x03,0x04 //Poll For Card \n");
-    		printf("4) 0x5F,0x84,0x81,0x15,0x06,0xFE,0x00,0x84,0x00,0x00,0x08 //Straight Through Mode \n");
+		printf("Select Command \n");
+		printf("1) 0x5F,0x81,0x81,0x01,0x02,0x00,0x00 //Antenna Off \n");
+		printf("2) 0x5F,0x81,0x81,0x01,0x06,0x04,0x00,0x01,0x02,0x03,0x04 //Load Polling Table \n");
+		printf("3) 0x5F,0x81,0x81,0x01,0x06,0x02,0x00,0x01,0x02,0x03,0x04 //Poll For Card \n");
+		printf("4) 0x5F,0x84,0x81,0x15,0x06,0xFE,0x00,0x84,0x00,0x00,0x08 //Straight Through Mode \n");
 
-			printf("\n\nEnter Command 1,2,3 or 4: ");
-			scanf("%s" , command);
-    	} else {
-    		printf("\nReSending Command : %s\n",tlvtest4);
+		printf("\n\nEnter Command 1,2,3 or 4: ");
+		scanf("%s" , command);
+
+    	struct timeval start, end;
+    	double elapsedTime;
+    	gettimeofday(&start, NULL);
+
+    	if (command[0] == '1'){
+    		 //Send some data
+			if( send(sock , tlvtest1 , sizeof(tlvtest1) , MSG_NOSIGNAL) <= 0)
+			{
+				puts("Send failed");
+				return 1;
+			}
     	}
-
-    	printf("\nLENGTH : %d\n",sizeof(tlvtest4));
-
-    	resend = 0;
-
-        //Send some data
-        if( send(sock , tlvtest4 , sizeof(tlvtest4) , MSG_NOSIGNAL) <= 0)
-        {
-            puts("Send failed");
-            return 1;
+      	if (command[0] == '2'){
+      		//Send some data
+			if( send(sock , tlvtest2 , sizeof(tlvtest2) , MSG_NOSIGNAL) <= 0)
+			{
+				puts("Send failed");
+				return 1;
+			}
         }
+      	if (command[0] == '3'){
+      		//Send some data
+			if( send(sock , tlvtest3 , sizeof(tlvtest3) , MSG_NOSIGNAL) <= 0)
+			{
+				puts("Send failed");
+				return 1;
+			}
+        }
+      	if (command[0] == '4'){
+      		//Send some data
+			if( send(sock , tlvtest4 , sizeof(tlvtest4) , MSG_NOSIGNAL) <= 0)
+			{
+				puts("Send failed");
+				return 1;
+			}
+        }
+
+
+        gettimeofday(&end, NULL);
+
+        // compute and print the elapsed time in millisec
+            elapsedTime = (end.tv_sec - start.tv_sec) * 1000.0;      // sec to ms
+            elapsedTime += (end.tv_usec - start.tv_usec) / 1000.0;   // us to ms
+            printf("Time is : %fms\n", elapsedTime);
 
         memset(&server_reply, 0, sizeof(server_reply));
 
@@ -95,7 +126,7 @@ start:
             close(sock);
             resend = 1;
             goto start;
-           // break;
+            break;
         }
 
         printf("R-APDU : ");
